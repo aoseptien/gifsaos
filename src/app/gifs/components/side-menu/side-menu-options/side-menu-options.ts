e6d4df11801b904dfbfede606c17b223375f12ef
Alejandro Ogando <aoseptien@gmail.com>
2025-12-15 21:42:20 -0500

    feat: enhance side menu and dashboard layout, implement dynamic routing and environment variables

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface SideMenuOption {
+  label: string;
+  icon: string;
+  route: string;
+  subLabels: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
 })
-export class SideMenuOptions {}
+export class SideMenuOptions {
+  menuOptions: SideMenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: '/dashborad/trending',
+      subLabels: 'Trending Gifs',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      route: '/dashborad/search',
+      subLabels: 'Search Gifs',
+    },
+    // {
+    //   icon: 'fa-solid fa-clock-rotate-left',
+    //   label: 'History',
+    //   route: '/dashborad/history',
+    //   subLabels: 'Recent Searches',
+    // },
+  ];
+}
